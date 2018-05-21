11846ef3f9ed65e1759fdffce2d6b6231b11a77c
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-05-21 23:01:38 +0200

    private

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Supplier.create(company: "Framtiden", company_number: nil, company_representative: nil, second_representative: nil, email: "petter.fagerlund@gmail.com", telephone: nil, city: "göteborg", recruitment_percentage: "25", description: "Vi är bäst på unga talanger med upp till 5 års erf...")
+
+Supplier.create(company: "Effektiv", company_number: nil, company_representative: nil, second_representative: nil, email: "petter.fagerlund@gmail.com", telephone: nil, city: "göteborg", recruitment_percentage: "30", description: "För vinnare i arbetslivet")
